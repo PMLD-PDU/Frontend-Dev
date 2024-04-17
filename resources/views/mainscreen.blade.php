5b0b741f1ae4c5e473871d94b9b320c9802bc1b3
Mumtaza15 <ahmad.fatha1502@mail.ugm.ac.id>
2024-04-17 19:06:32 +0700

    first commit login & mainscreen page

---
+++ resources/views/mainscreen.blade.php
@@ -0,0 +1,135 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <title>Mainscreen PDU</title>
+    <style>
+    body {
+        background-image: url('img_background.jpg');
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+        background-size: cover;
+        font-family: 'Montserrat';
+    }
+
+    .card{
+        border-radius: 10%;
+    }
+
+    .login-title{
+        color: #E75C33;
+        font-weight: 800;
+        font-size: 28px;
+    }
+    
+    .login-content{
+        font-weight: 500;
+    }
+
+    .btn{
+        font-weight: 700;
+    }
+
+    .form-control{
+        background-color: #FCEBE7;
+    }
+
+    .btn-color{
+        background-color: #E75C33;
+        color: #fff;
+    }
+
+    /* .btn:hover{
+        color: #FCEBE7;
+    } */
+    </style>
+  </head>
+  <body>
+    {{-- <div class="container d-flex flex-column min-vh-100">
+
+        <div class="col d-flex justify-content-center">
+
+            <div class="card text-center" style="width: 18rem; height: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Special title treatment</h5>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+    
+    </div> --}}
+
+    <div class="container">
+        <div class="row">
+          <div class="col-md-6 offset-md-3">
+            <div class="card my-5 shadow p-3">
+    
+              <form class="card-body p-lg-5">
+    
+                <div class="text-center">
+                  <img src="img_pdu.jpg" class="img-fluid profile-image-pic img-thumbnail rounded-circle mb-3 shadow p-1"
+                    width="80px" alt="profile">
+                </div>
+    
+                <div class="mb-1 login-title">
+                    <i class="fa fa-desktop"></i>
+                    Real Time Monitoring
+                </div>
+
+                <div class="form-group row my-2">
+                  <label for="staticEmail" class="col-sm-3 col-form-label">Company</label>
+                  <div class="form-group col-sm-9">
+                    <select id="inputState" class="form-control">
+                      <option selected>- Select -</option>
+                      <option>...</option>
+                    </select>
+                  </div>
+                </div>
+
+                <div class="form-group row my-2">
+                  <label for="inputPassword" class="col-sm-3 col-form-label">Well</label>
+                  <div class="form-group col-sm-9">
+                    <select id="inputState" class="form-control">
+                      <option selected>- Select -</option>
+                      <option>...</option>
+                    </select>
+                  </div>
+                </div>
+
+                <div class="d-grid gap-2 d-md-flex justify-content-md-end my-3">
+                  <button class="btn btn-color" type="button">View Studio</button>
+                </div>
+
+              </form>
+            </div>
+    
+          </div>
+        </div>
+      </div>
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('logintes');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/mainscreen', function () {
+    return view('mainscreen');
 });
